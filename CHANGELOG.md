d4795b520b23c2f6bb3268bf4040c717eb033de2
Kolja Dummann <kolja@hey.com>
2021-04-12 10:26:19 +0200

    better compatibility with projector

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,22 @@
 
 ## [Unreleased]
 
+Compatibility with Projector
+
+### Added
+
+### Changed
+- User classes instrumented by Projector to open the browser. 
+### Deprecated
+
+### Removed
+
+### Fixed
+
+### Security
+
+## [1.0]
+
 First release to **JetBrains** marketplace.
 
 ### Added
--- src/main/kotlin/IdeIntegration.kt
+++ src/main/kotlin/IdeIntegration.kt
@@ -1,5 +1,6 @@
 package ws.logv.fernsprecher
 
+import com.intellij.ide.BrowserUtil
 import com.intellij.ide.browsers.BrowserLauncher
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
@@ -12,6 +13,6 @@ class OpenLogsInBrowserAction: AnAction("Open Logs in Browser") {
         val serverPort = BuiltInServerOptions.getInstance().effectiveBuiltInServerPort
         val authority = "localhost:$serverPort"
         val url = Urls.newHttpUrl(authority, "/fernsprecher/")
-        BrowserLauncher.instance.browse(url.toExternalForm(), project = e.project)
+        BrowserUtil.browse(url.toExternalForm())
     }
 }
